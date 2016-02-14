7a4b63826ac042b0766f8c9d94b97e3d90e34ea3
Chris Pope <cpope13@gmail.com>
2016-02-13 16:51:02 -0800

    add necessary files for heroku app

--- run.py
+++ run.py
@@ -1,11 +1,37 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
   app.run(debug=True)
